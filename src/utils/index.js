57e29f12544fc99ff8893fbddefbc2dba2cc5ef2
aemxdp <aemxdp@gmail.com>
2017-08-10 21:51:35 +0300

    Add utils typings

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,29 +1,41 @@
+// @flow
+
 import ThemeManager from './ThemeManager';
 import MidiManager from './MidiManager';
 import DelayList from './DelayList';
+
+type Point = {
+    x: number;
+    y: number;
+};
+
+type Shape = Point & {
+    width: number;
+    height: number;
+}
 
 export default {
     ThemeManager,
     MidiManager,
     DelayList,
     text: {
-        snakeToCamel: (string) =>
-            string.replace(/(-.)/g, (m) => m[1].toUpperCase()),
-        offset: (n, ...offsets) =>
+        snakeToCamel: (s: string) =>
+            s.replace(/(-.)/g, (m) => m[1].toUpperCase()),
+        offset: (n: number, ...offsets: number[]) =>
             offsets[Math.max(`${n}`.length - 1, 0)] ||
             offsets[offsets.length - 1],
     },
     geometry: {
-        center: (shape, offset = { x: 0, y: 0 }) => ({
+        center: (shape: Shape, offset: Point = { x: 0, y: 0 }) => ({
             x: offset.x + shape.x + shape.width / 2,
             y: offset.y + shape.y + shape.height / 2,
         }),
     },
     events: {
-        propagationStopper: (e) => e.stopPropagation(),
+        propagationStopper: (e: Event) => e.stopPropagation(),
     },
     music: {
-        noteToMs: (beats, noteFraction, bpm) =>
+        noteToMs: (beats: number, noteFraction: number, bpm: number) =>
             ((60000 / (bpm / 4)) / noteFraction) * beats
     }
 }
