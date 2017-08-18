2ae0776f60ea57c2a4176fb9446879a1242b40a4
aemxdp <aemxdp@gmail.com>
2017-08-18 12:56:08 +0300

    Implement arrayToIdMap utility function

--- src/utils/objectUtils.ts
+++ src/utils/objectUtils.ts
@@ -1,3 +1,9 @@
+export type WithId<T> = T & { id: number };
+
+export function arrayToIdMap<T>(objects: WithId<T>[]): { [id: number]: WithId<T> } {
+    return objects.reduce((acc, b) => (acc[b.id] = b) && acc, {});
+}
+
 export function objectValues<T>(object: { [key: string]: T }): T[] {
     return (Object as any).values(object);
 }
