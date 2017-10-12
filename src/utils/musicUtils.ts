39a25fecbc71e4882c697cabce3f4b0bc06bf3e6
aemxdp <aemxdp@gmail.com>
2017-10-12 17:31:53 +0300

    Implement textNoteToMidiNote

--- src/utils/musicUtils.ts
+++ src/utils/musicUtils.ts
@@ -1,3 +1,30 @@
+const NOTE_INDEX = {
+    'C': 0,
+    'D': 2,
+    'E': 4,
+    'F': 5,
+    'G': 7,
+    'A': 9,
+    'B': 11,
+};
+
+const ACCIDENTAL = {
+    '#': +1,
+    'b': -1,
+};
+
+export function textNoteToMidiNote(note: string): number {
+    const match = (/(\w)(-?\d)?([#b])?/g).exec(note);
+    if (match) {
+        const noteIndex = NOTE_INDEX[match[1]];
+        const octave = parseInt(match[2], 10) || 3;
+        const accidental = ACCIDENTAL[match[3]] || 0;
+        return 24 + 12 * octave + noteIndex + accidental;
+    } else {
+        throw new Error('Unknown note!');
+    }
+}
+
 export function noteToMs(beats: number, noteFraction: number, bpm: number): number {
     return ((60000 / (bpm / 4)) / noteFraction) * beats;
 }
